8b4b0e5c8e91644620fb49713cd94c839cb3eca7
walid <walid.sa3d@gmail.com>
2024-07-30 18:10:54 +0100

    refactor code

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,18 +1,8 @@
 import os
 
-import testinfra.utils.ansible_runner as runner
-
-testinfra_hosts = runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
 def test_fail2ban_installed(host):
     pkg = host.package("fail2ban")
     assert pkg.is_installed
-
-def test_fail2ban_service_is_running(host):
-     with host.sudo():
-        svc = host.service('fail2ban')
-        assert svc.is_running
 
 def test_fail2ban_service_is_enabled(host):
     service = host.service("fail2ban")
